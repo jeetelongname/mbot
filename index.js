515c5eaadd12adc97abc58da960b6ea78782ae50
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-05-22 16:24:13 +0100

    feat: code cleanup and f command

--- index.js
+++ index.js
@@ -1,38 +1,77 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const guildID = "711576565112373319";
+
+const commands = {
+  hello: {
+    description: "hello world",
+    function: (interaction, _args) => {
+      slashReturnText(interaction, "hello world!");
+    },
+  },
+
+  f: {
+    description: "reply with f",
+    function: (interaction, _args) => {
+      slashReturnText(
+        interaction,
+        `
+⠀⠀⠀⢀⡤⢶⣶⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣠⣤⣤⣤⣿⣧⣀⣀⣀⣀⣀⣀⣀⣀⣤⡄⠀
+⢠⣾⡟⠋⠁⠀⠀⣸⠇⠈⣿⣿⡟⠉⠉⠉⠙⠻⣿⡀
+⢺⣿⡀⠀⠀⢀⡴⠋⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠙⠇
+⠈⠛⠿⠶⠚⠋⣀⣤⣤⣤⣿⣿⣇⣀⣀⣴⡆⠀⠀⠀
+⠀⠀⠀⠀⠠⡞⠋⠀⠀⠀⣿⣿⡏⠉⠛⠻⣿⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠈⠁⠀⠀
+⠀⠀⣠⣶⣶⣶⣶⡄⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⢰⣿⠟⠉⠙⢿⡟⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⡟⠀⠀⠀⠘⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠈⢿⡄⠀⠀⠀⠀⠀⣼⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠙⠷⠶⠶⠶⠿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+`
+      );
+    },
+  },
+};
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 
-  client.api
-    .applications(client.user.id)
-    .guilds(711576565112373319)
-    .commands.post({
-      data: {
-        name: "hello",
-        description: "hello world command",
-        // possible options here e.g. options: [{...}]
-      },
-    });
+  for (const command of Object.keys(commands)) {
+    slashSimpleDefine(command, commands[command].description);
+  }
 
   client.ws.on("INTERACTION_CREATE", async (interaction) => {
     const command = interaction.data.name.toLowerCase();
     const args = interaction.data.options;
 
-    if (command === "hello") {
-      // here you could do anything. in this sample
-      // i reply with an api interaction
-      client.api.interactions(interaction.id, interaction.token).callback.post({
-        data: {
-          type: 4,
-          data: {
-            content: "hello world!!!",
-          },
-        },
-      });
-    }
+    commands[command].function(interaction, args);
   });
 });
+
+function slashReturnText(interaction, text) {
+  client.api.interactions(interaction.id, interaction.token).callback.post({
+    data: {
+      type: 4,
+      data: {
+        content: text,
+      },
+    },
+  });
+}
+
+function slashSimpleDefine(name, description) {
+  client.api
+    .applications(client.user.id)
+    .guilds(guildID)
+    .commands.post({
+      data: {
+        name: name,
+        description: description,
+        // possible options here e.g. options: [{...}]
+      },
+    });
+}
 
 client.login(process.env.TOKEN);
