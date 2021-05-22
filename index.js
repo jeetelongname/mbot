e90bcfd30479afa1b8a8f4589c5857896c405b1d
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-05-22 14:58:06 +0100

    feat: adding basic slash command

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
-});
 
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+  client.api
+    .applications(client.user.id)
+    .guilds(711576565112373319)
+    .commands.post({
+      data: {
+        name: "hello",
+        description: "hello world command",
+        // possible options here e.g. options: [{...}]
+      },
+    });
+
+  client.ws.on("INTERACTION_CREATE", async (interaction) => {
+    const command = interaction.data.name.toLowerCase();
+    const args = interaction.data.options;
+
+    if (command === "hello") {
+      // here you could do anything. in this sample
+      // i reply with an api interaction
+      client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: {
+            content: "hello world!!!",
+          },
+        },
+      });
+    }
+  });
 });
 
 client.login(process.env.TOKEN);
